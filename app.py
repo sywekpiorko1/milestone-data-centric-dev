d2ea0cea5025b7d94e39706d5d76074c2134ad71
sywekpiorko1 <termetal@gmail.com>
2019-03-23 21:11:59 +0000

    Add insert_recipe() root to push data into MongoDB, add style for checkboxes in add_recipe.html, cleanup html

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 # Collections
+category_collection = mongo.db.category_collection
 recipes_collection = mongo.db.recipes_collection
 allergens_collection = mongo.db.allergens_collection
 cuisine_collection = mongo.db.cuisine_collection
@@ -33,15 +34,37 @@ def get_recipes():
 
 @app.route('/addRecipe')
 def add_recipe():
+    _categories = category_collection.find()
+    category_list = [category for category in _categories]
     _allergens = allergens_collection.find()
     allergen_list = [allergen for allergen in _allergens]
     _cuisines = cuisine_collection.find()
     cuisine_list = [cuisine for cuisine in _cuisines]
     return render_template('add_recipe.html',
+                            categories=category_list,
                             allergens=allergen_list,
                             cuisines=cuisine_list)
 
-# {{ url_for('insert_recipe') }}
+
+@app.route('/insertRecipe', methods = ['POST'])
+def insert_recipe():
+    
+    form_request_to_dict = request.form.to_dict()
+    # as allergens collected from checboxes add them to list
+    form_request_to_dict['allergens'] = request.form.getlist('allergens')
+    # split ingredients to create list of them
+    form_request_to_dict['ingredients'] = form_request_to_dict['ingredients'].split(",")    
+    # add keys to dictionary to be used when searching and sorting
+    form_request_to_dict['views'] = 0
+    form_request_to_dict['upvotes'] = 0
+    form_request_to_dict['upvotes_who'] = []
+
+    print(form_request_to_dict)
+
+    recipes = recipes_collection
+    recipes.insert_one(form_request_to_dict)
+    
+    return redirect(url_for('get_recipes'))
 
 
 @app.route('/viewRecipe/<recipe_id>')
