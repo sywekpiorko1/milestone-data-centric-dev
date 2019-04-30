906ba16665ed68c9fa4c47f8a62a69e70f9caa29
sywekpiorko1 <termetal@gmail.com>
2019-04-30 20:07:31 +0100

    Add graphs showing count of recipes by criteria

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, redirect, request, url_for, session, f
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+import pygal
+from pygal.style import LightSolarizedStyle
 import json
 import pprint
 from operator import itemgetter
@@ -68,6 +70,7 @@ def index():
 
 ############################### RECIPES ###############################
 
+
 @app.route('/recipes')
 def get_recipes():
     viewer = 'anonim'
@@ -110,6 +113,7 @@ def get_recipes():
                            viewer=viewer)
 
 ############################### VIEW RECIPE ###############################
+
 
 @app.route('/viewRecipe/<recipe_id>')
 def view_recipe(recipe_id):
@@ -161,6 +165,7 @@ def view_recipe(recipe_id):
 
 ############################### VOTE UP RECIPES ###############################
 
+
 @app.route('/voteUp/<recipe_id>/<viewer>')
 def vote_up(recipe_id, viewer):
     # Add viewer name to upvotes_who[]
@@ -186,6 +191,7 @@ def vote_up(recipe_id, viewer):
 
 ############################### VOTE DOWN RECIPE ###############################
 
+
 @app.route('/removeVoteUp/<recipe_id>/<viewer>')
 def remove_vote_up(recipe_id, viewer):
     # Remove viewer name to upvotes_who[]
@@ -210,6 +216,7 @@ def remove_vote_up(recipe_id, viewer):
     return redirect(request.referrer)
 
 ############################### FILTER RECIPES ###############################
+
 
 @app.route('/filterRecipes', methods=['GET', 'POST'])
 def filter_recipes():
@@ -237,7 +244,8 @@ def filter_recipes():
                 search_filter[key] = form[key]
                 filters.append(search_filter)
 
-        print(f"Filter (category or cuisine) :{filters}) | Filters (do not display allergens) :{exclusion}")    
+        print(
+            f"Filter (category or cuisine) :{filters}) | Filters (do not display allergens) :{exclusion}")
 
         # check if selectoin made - if not - display warning
         if filters or exclusion:
@@ -262,19 +270,20 @@ def filter_recipes():
             count = len(filtered_and_excluded_allergens)
 
             p_limit = 8
-            recipes = (filtered_and_excluded_allergens)[p_offset:p_limit+p_offset]
+            recipes = (filtered_and_excluded_allergens)[
+                p_offset:p_limit+p_offset]
 
             return render_template("recipes.html",
-                                allergens=allergens_collection.find(),
-                                categories=category_list_sorted,
-                                count=count,
-                                cuisines=cuisine_list_sorted,
-                                next_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
-                                prev_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
-                                p_limit=p_limit,
-                                p_offset=p_offset,
-                                recipes=recipes,
-                                title="Filtered Recipes")
+                                   allergens=allergens_collection.find(),
+                                   categories=category_list_sorted,
+                                   count=count,
+                                   cuisines=cuisine_list_sorted,
+                                   next_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
+                                   prev_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
+                                   p_limit=p_limit,
+                                   p_offset=p_offset,
+                                   recipes=recipes,
+                                   title="Filtered Recipes")
         else:
             flash("Choose something before hit FILTER")
             return redirect(request.referrer)
@@ -284,7 +293,7 @@ def filter_recipes():
         try:
             p_limit = int(request.args['limit'])
         except:
-            p_limit=8
+            p_limit = 8
         # Request the offset from link
         # make sure is 0 or more to avoid erver error
         try:
@@ -301,18 +310,19 @@ def filter_recipes():
         recipes = (filtered_and_excluded_allergens)[p_offset:p_limit+p_offset]
 
         return render_template("recipes.html",
-                            allergens=allergens_collection.find(),
-                            categories=category_list_sorted,
-                            count=count,
-                            cuisines=cuisine_list_sorted,
-                            next_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
-                            prev_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
-                            p_limit=p_limit,
-                            p_offset=p_offset,
-                            recipes=recipes,
-                            title="Filtered Recipes")
+                               allergens=allergens_collection.find(),
+                               categories=category_list_sorted,
+                               count=count,
+                               cuisines=cuisine_list_sorted,
+                               next_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
+                               prev_url=f"/filterRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
+                               p_limit=p_limit,
+                               p_offset=p_offset,
+                               recipes=recipes,
+                               title="Filtered Recipes")
 
 ############################### SEARCH RECIPES ###############################
+
 
 @app.route('/searchRecipes', methods=['GET', 'POST'])
 def search_recipes():
@@ -328,26 +338,28 @@ def search_recipes():
 
             global searchedRecipes
             searchedRecipes = []
-            searchedRecipes = list(recipes_collection.find({ "$and" : [{ 'meal' :{'$regex':inMealWord, '$options':'i'}}, { 'ingredients' :{'$regex':inIngredWord, '$options':'i'}} ]}))
+            searchedRecipes = list(recipes_collection.find({"$and": [{'meal': {'$regex': inMealWord, '$options': 'i'}}, {
+                                   'ingredients': {'$regex': inIngredWord, '$options': 'i'}}]}))
             p_offset = 0
             count = len(searchedRecipes)
-            
+
             p_limit = 8
             recipes = (searchedRecipes)[p_offset:p_limit+p_offset]
 
-            print(f"Type of results is  {type(searchedRecipes)} and it has {len(searchedRecipes)} items")
+            print(
+                f"Type of results is  {type(searchedRecipes)} and it has {len(searchedRecipes)} items")
             return render_template("recipes.html",
-                                    allergens=allergens_collection.find(),
-                                    categories=category_list_sorted,
-                                    count=count,
-                                    cuisines=cuisine_list_sorted,
-                                    next_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
-                                    prev_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
-                                    p_limit=p_limit,
-                                    p_offset=p_offset,
-                                    recipes=recipes,
-                                    title="Searched Recipes")
-            
+                                   allergens=allergens_collection.find(),
+                                   categories=category_list_sorted,
+                                   count=count,
+                                   cuisines=cuisine_list_sorted,
+                                   next_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
+                                   prev_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
+                                   p_limit=p_limit,
+                                   p_offset=p_offset,
+                                   recipes=recipes,
+                                   title="Searched Recipes")
+
         else:
             flash("Type in something before hit SEARCH")
             return redirect(request.referrer)
@@ -374,18 +386,19 @@ def search_recipes():
         recipes = (searchedRecipes)[p_offset:p_limit+p_offset]
 
         return render_template("recipes.html",
-                                    allergens=allergens_collection.find(),
-                                    categories=category_list_sorted,
-                                    count=count,
-                                    cuisines=cuisine_list_sorted,
-                                    next_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
-                                    prev_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
-                                    p_limit=p_limit,
-                                    p_offset=p_offset,
-                                    recipes=recipes,
-                                    title="Searched Recipes")
+                               allergens=allergens_collection.find(),
+                               categories=category_list_sorted,
+                               count=count,
+                               cuisines=cuisine_list_sorted,
+                               next_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset + p_limit)}",
+                               prev_url=f"/searchRecipes?limit={str(p_limit)}&offset={str(p_offset - p_limit)}",
+                               p_limit=p_limit,
+                               p_offset=p_offset,
+                               recipes=recipes,
+                               title="Searched Recipes")
 
 ############################### ADD RECIPE ###############################
+
 
 @app.route('/addRecipe')
 def add_recipe():
@@ -435,6 +448,7 @@ def insert_recipe():
 
 ############################### EDIT RECIPE ###############################
 
+
 @app.route('/editRecipe/<recipe_id>')
 def edit_recipe(recipe_id):
             # Check if user is logged in
@@ -479,6 +493,7 @@ def edit_recipe(recipe_id):
 
 ############################### UPDATE RECIPE ###############################
 
+
 @app.route('/updateRecipe/<recipe_id>', methods=['POST'])
 def update_recipe(recipe_id):
     form_request_to_dict = request.form.to_dict()
@@ -503,6 +518,7 @@ def update_recipe(recipe_id):
     return redirect(url_for('view_recipe', recipe_id=recipe_id))
 
 ############################### DELETE RECIPE ###############################
+
 
 @app.route('/deleteRecipe/<recipe_id>', methods=['GET', 'POST'])
 def delete_recipe(recipe_id):
@@ -538,6 +554,7 @@ def login():
 
 ############################### USER AUTH ###############################
 
+
 @app.route('/user_auth', methods=['POST'])
 def user_auth():
     form = request.form.to_dict()
@@ -563,6 +580,7 @@ def user_auth():
         return redirect(url_for('register'))
 
 ############################### REGISTER ###############################
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -611,6 +629,7 @@ def register():
 
 ############################### LOG OUT ###############################
 
+
 @app.route('/logout')
 def logout():
     # Clear the session
@@ -619,6 +638,7 @@ def logout():
     return redirect(url_for('index'))
 
 ############################### PROFILE PAGE ###############################
+
 
 @app.route('/profile/<user>')
 def profile(user):
@@ -670,6 +690,7 @@ def profile(user):
 
 ############################### ADMIN AREA ###############################
 
+
 @app.route('/admin')
 def admin():
     if 'user' in session:
@@ -684,6 +705,7 @@ def admin():
 
 ############################### ERROR 400 ###############################
 
+
 @app.errorhandler(400)
 def bad_request(e):
     return render_template('400.html'), 400
@@ -697,10 +719,80 @@ def page_not_found(e):
 
 ############################### ERROR 500 ###############################
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     session.clear()
     return render_template('500.html'), 500
+
+######################################################################
+#                             GRAPHS
+######################################################################
+
+
+@app.route('/chart')
+def chart():
+
+    # Request the chart key from link
+    count_key  = str(request.args['count_key'])
+
+    distinctKey = (recipes_collection.distinct(str(count_key)))
+    distinctKey.sort()
+    print(len(distinctKey))
+    chart = pygal.Bar(print_values=True,
+                        print_values_position='top',
+                        legend_at_bottom=True,
+                        style=LightSolarizedStyle(value_font_family='googlefont:Raleway',
+                                        value_font_size=20,
+                                        value_colors=()))
+    chart.title = f"Recipes count by {count_key}"
+    
+    for item in distinctKey:
+        count = recipes_collection.find({count_key: item}).count()
+        chart.add(item, count)
+        print(item, count)
+
+    return render_template('graph.html', graph=chart.render_data_uri(), title="Graphs")
+
+
+@app.route('/chartComplex')
+def chart_complex():
+
+    # Request the x and y streams from link
+    factor_x  = str(request.args['factor_x'])
+    factor_y = str(request.args['factor_y'])
+
+
+    factor_x_list = (recipes_collection.distinct(factor_x))
+    factor_x_list.sort()
+
+    factor_y_list = (recipes_collection.distinct(factor_y))
+    factor_y_list.sort()
+
+    line_chart = pygal.Line(x_label_rotation=45, 
+                            interpolate='hermite', 
+                            legend_at_bottom=True,
+                            style=LightSolarizedStyle(value_font_family='googlefont:Raleway',
+                                            value_font_size=20,
+                                            value_colors=('black',)))
+    line_chart.title = f"{factor_x} in {factor_y}"
+
+    print(factor_y_list)
+    line_chart.x_labels = factor_y_list
+
+    for x in factor_x_list:
+        counts = []
+
+        for y in factor_y_list:
+            count = recipes_collection.find(
+                {"$and": [{factor_x: x}, {factor_y: y}]}).count()
+            counts.append(count)
+        print(x, counts)
+        line_chart.add(x, counts)
+
+    line_chart.render()
+
+    return render_template('graph.html', graph=line_chart.render_data_uri(), title="Graphs")
 
 ######################################################################
 #                             MAIN APP
